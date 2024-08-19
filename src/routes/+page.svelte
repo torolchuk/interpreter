5f4614f8d88f649e18e549ccfd56ef293bf2be79
torolchuk <thmproj@gmail.com>
2024-08-19 10:57:26 +0300

    Input container: part 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
 <script>
-  import Header from "./Header.svelte";
+  import Header from "$lib/Header.svelte";
+  import Input from "$lib/Input.svelte";
 </script>
 
+<style>
+  .page-container {
+    height: calc(100% - 100px);
+    margin: 0 0 50px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .input-box {
+    margin-top: auto;
+  }
+</style>
+
 <Header />
+
+<div class="page-container">
+  <div class="input-box">
+    <Input />
+  </div>
+</div>
+
