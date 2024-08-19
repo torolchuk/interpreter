c633e9645d350bb941b0d085a155302c50daac1b
torolchuk <thmproj@gmail.com>
2024-08-19 10:38:39 +0300

    Header design

---
+++ src/routes/Header.svelte
@@ -0,0 +1,43 @@
+<script>
+
+</script>
+
+<style>
+  .header {
+    height: 60px;
+    background-color: rgba(255, 255, 255, .2);
+    backdrop-filter: blur(10px); 
+    box-shadow: 0 4px 16px 0 rgba(0, 0, 0, .05);
+    display: flex;
+    padding: 0 16px;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo {
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .github-link {
+    display: inline-block;
+  }
+
+  .github-icon {
+    height: 46px;
+  }
+
+</style>
+
+<div class="header">
+  <span class="logo">
+    Interpreter
+  </span>
+  <a
+    class="github-link"
+    href="https://github.com/torolchuk/interpreter"
+    target="_blank"
+  >
+    <img class="github-icon" src="/icons/github.png" alt="github icon">
+  </a>
+</div>
