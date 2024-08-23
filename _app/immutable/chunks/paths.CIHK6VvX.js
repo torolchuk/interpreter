466f4efedba559d35b058e300b22697d1d98120b
torolchuk <thmproj@gmail.com>
2024-08-23 22:36:38 +0300

    Updates

--- _app/immutable/chunks/paths.CIHK6VvX.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_dpd0l8)==null?void 0:s.base)??"/interpreter";var e;const a=((e=globalThis.__sveltekit_dpd0l8)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.BKsHwz38.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BuZuf1UR.js";export{t as start};
